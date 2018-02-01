9bc5cb2ee3ddd68f7d3d024dd5c4c51be38182ad
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-31 21:45:03 -0700

    d20 fixed

--- src/server.c
+++ src/server.c
@@ -187,10 +187,16 @@ int get_listener_socket(char *port)
  */
 int send_response(int fd, char *header, char *content_type, char *body)
 {
+  // char new_body[1000];
   const int max_response_size = 65536;
   char response[max_response_size];
   int response_length;
+  printf("\"%s\"", body);
+  printf(">>>%c\n", body[0]);
+  // body = "2";
+  // strcpy(new_body, "17");
   int body_length = strlen(body);
+  // body_length = 0;
 
   response_length = snprintf(response, max_response_size, "%s\n"
     "Connection: close\n"
@@ -200,9 +206,10 @@ int send_response(int fd, char *header, char *content_type, char *body)
     "%s",
     header, body_length, content_type, body
   );
-  printf("The response length: %i\n", response_length);
-  printf("The body length: %d\n", body_length);
-
+  // printf(">>%s\n", response);
+  // printf("The response length: %i\n", response_length);
+  // printf("The body length: %d\n", body_length);
+  // fflush(stdout);
   int rv = send(fd, response, response_length, 0);
 
   if (rv < 0) {
@@ -247,9 +254,9 @@ void get_d20(int fd)
   srand(time(NULL));
   int r = rand() % 20;
 
-  sprintf(response_body, "%i\n", r);
-  printf("The random number is: %i\n", r);
-
+  sprintf(response_body, "%02i", r);
+  // printf("The random number is: %i\n", r);
+  // printf("BODY: %s\n", response_body);
   send_response(fd, "HTTP/1.1 200 OK", "text/plain", response_body);
 }
 
@@ -314,9 +321,9 @@ void handle_http_request(int fd)
   // call the appropriate handler functions, above, with the incoming data
   if (strcmp(request_path, "/") == 0) {
     get_root(fd);
-  } else if (strcmp(request_path, "/d20")) {
+  } else if (strcmp(request_path, "/d20") == 0) {
     get_d20(fd);
-  } else if (strcmp(request_path, "/date")) {
+  } else if (strcmp(request_path, "/date") == 0) {
     get_date(fd);
   }
 
