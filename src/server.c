406ff753f47186f3f5319a2d18df06669b646cfe
totsuya <supertatortots@gmail.com>
2018-06-25 18:16:28 -0400

    added GET handler

--- src/server.c
+++ src/server.c
@@ -302,6 +302,9 @@ void handle_http_request(int fd)
 
   // !!!! IMPLEMENT ME
   // call the appropriate handler functions, above, with the incoming data
+  if (strcmp(request_path, "/") == 0) {
+    get_root(fd); //GET file directory (i think)
+  }
 }
 
 /**
