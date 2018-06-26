a623f74f54f48df45b642802d7bafca3c87ca657
Nedu Robert <nedumrobert@gmail.com>
2018-06-26 15:48:16 +0100

    called the appropriate handler based on the request type and path

--- src/server.c
+++ src/server.c
@@ -294,6 +294,25 @@ void handle_http_request(int fd)
 
   // !!!! IMPLEMENT ME
   // call the appropriate handler functions, above, with the incoming data
+
+  if(strcmp(request_type, "GET") == 0) {
+    if (strcmp(request_path, "/") == 0)
+    {
+      get_root(fd);
+    }
+    else if(strcmp(request_type, "/date") == 0)
+    {
+      get_date(fd);
+    }
+    else if(strcmp(request_type, "/d20") == 0)
+    {
+      get_d20(fd);
+    }
+    else
+    {
+      resp_404(fd);
+    }
+  }
 }
 
 /**
