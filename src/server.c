50a5986bc0f990d6ca02ad5f095c0a6b21e810a7
Bradiowave <divebombwalker@gmail.com>
2018-08-13 12:47:06 -0700

    implemented input parsing and calling appropriate handler functions

--- src/server.c
+++ src/server.c
@@ -287,12 +287,29 @@ void handle_http_request(int fd)
   // !!!! IMPLEMENT ME
   // Get the request type and path from the first line
   // Hint: sscanf()!
+  sscanf( request, "%s %s %s", request_type, request_path, request_protocol);
 
   // !!!! IMPLEMENT ME (stretch goal)
   // find_start_of_body()
 
   // !!!! IMPLEMENT ME
   // call the appropriate handler functions, above, with the incoming data
+  if ( strcmp(*request_type, "GET") == 0 ) 
+  {
+    if ( strcmp(*request_path, "/") == 0 ) {
+      get_root(fd);
+    } else if ( strcmp(*request_path, "/d20") == 0 ) {
+      get_d20(fd);
+    } else if ( strcmp(*request_path, "/date") == 0 ) {
+      get_date(fd);
+    } else {
+      resp_404(fd);
+    }
+  } 
+  else if ( strcmp(*request_type, "POST") == 0 ) 
+  {
+    // !!!! Stretch Goal (I think)
+  }
 }
 
 /**
