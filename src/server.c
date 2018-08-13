1bed1a520e83e2dcadac7a6bce788f2c7820f592
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-13 15:29:52 -0400

    -scanf() implemented to request type and path on the first line

--- src/server.c
+++ src/server.c
@@ -287,12 +287,15 @@ void handle_http_request(int fd)
   // !!!! IMPLEMENT ME
   // Get the request type and path from the first line
   // Hint: sscanf()!
+  sscanf(request, "%s %s %s", request_type, request_path, request_protocol);
+
 
   // !!!! IMPLEMENT ME (stretch goal)
   // find_start_of_body()
 
   // !!!! IMPLEMENT ME
   // call the appropriate handler functions, above, with the incoming data
+
 }
 
 /**
