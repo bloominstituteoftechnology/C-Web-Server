b09bf5d14d1fde03f30e22a3e9ff7813020cc9ad
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-08-20 12:47:56 -0700

    get_listener_socket

--- src/retyped_server.c
+++ src/retyped_server.c
@@ -61,3 +61,84 @@ void *get_in_addr(struct sockaddr *sa)
 
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
+
+// returns the main listening socket, returns -1 or error
+int get_listener_socket(char *port)
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int yes=1;
+    int rv;
+
+    // This code block lookds at the local network interfaces and
+    // tries to find some that match our requirements(either IPv4 or
+    // IPv6(AF_UNSPEC) and TCP(SOCK_STREAM) and use any IP on this
+    // machine (AI_PASSIVE)).
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) !=0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_stderror(rv));
+        return -1;
+    }
+
+    // Once we have a list of potention interfaces, loop through
+    // them and try to set up a socket on each. Quit looping the 
+    // first time we have success.
+    for(p=servinfo; p!=NULL; p=p->ai_next)
+    {
+        // try and make a socket based on this canidate interface
+        if ((sockfd=socket(p->ai_family, p->ai_socktype,
+        p->ai_protocol)) == -1)
+        {
+            continue;
+        }
+    
+
+    // SO_REUSEADDR prevents the "address already in use" errors
+    // that commonly come up when testing servers.
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+        sizeof(int)) == -1)
+        {
+            perror("setsockopt");
+            close(sockfd);
+            freeaddrinfo(servinfo); // all done with this structure
+            return -2;
+        }
+
+    // See if we can bind this socket to this local IP address. This
+    // associates the file descriptor(socket discritor) thjat we will
+    // read and write on with a specific IP address.
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+    {
+        close(sockfd);
+        continue;
+    }
+
+    // if we got here, we have a bound socket and we're done.
+    break;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure.
+
+    // if p is null, it means we didnt break out of our loop, and
+    // therefor we have a bad socket.
+    if (p == NULL)
+    {
+        fprintf(stderr, "webserver: failed to find local address\n");
+        return -3;
+    }
+
+    // start listening. This is what allows remote computers to 
+    // connect to this socket/IP
+    if (listen(sockfd, BACKLOG) == -1)
+    {
+        close(sockfd);
+        return -4;
+    }
+
+    return sockfd;
+}
