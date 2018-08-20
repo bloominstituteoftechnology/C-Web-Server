49691b1b6865dd678c1951dda4e88187515bc3b5
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-08-20 12:24:08 -0700

    get_in_addr

--- src/retyped_server.c
+++ src/retyped_server.c
@@ -49,3 +49,15 @@ void start_reaper(void)
         exit(1);
     }
 }
+
+// This gets an Internet address, either IPv4 or IPv6.
+// Helper function to make printing easier
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
