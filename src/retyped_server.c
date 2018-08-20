a71e2f3c28714e53637715d0d174ba4da087684f
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-08-20 12:05:55 -0700

    initial commit

---
+++ src/retyped_server.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/file.h>
+#include <fcntl.h>
+
+#define PORT "3490" // the local host users will connect to
+
+#define BACKLOG 10 // capacity for pending connections queue
