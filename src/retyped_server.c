fa1fd09fb67f75e0de7f443f3618572208d81b6f
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-08-20 12:14:16 -0700

    sigchld_handler

--- src/retyped_server.c
+++ src/retyped_server.c
@@ -13,7 +13,20 @@
 #include <time.h>
 #include <sys/file.h>
 #include <fcntl.h>
-
 #define PORT "3490" // the local host users will connect to
-
 #define BACKLOG 10 // capacity for pending connections queue
+
+// handle SIGCHILD signal-We get this signal when a child process dies. This
+// function waits() for Zombie processes. This is only necessary if we've 
+// implemented a multiproccessed version with fork().
+void sigchld_handler(int s)
+{
+    (void)s; // quiet unused variable warning
+
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+    // wait for all the children that have died, discard the exit status
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+    // I believe this is errno is restored shold waitpid() overwrites it. 
+    errno = saved_errno;
+}
