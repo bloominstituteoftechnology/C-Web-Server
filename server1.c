d977e865e89b88ea76b1f373f0a2ae92599a9651
april7229 <martinezapril56@yahoo.com>
2018-08-20 19:24:29 -0400

    workingontyping

--- server1.c
+++ server1.c
@@ -81,3 +81,62 @@ void start_reaper(void)
  *
  * Helper function to make printing easier.
  */
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET){
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)sa)-> sin6_addr);
+}
+
+//**
+ * Return the main listening socket
+ * 
+ * Returns -1 or error
+ * /
+
+int get_listener_socket(char *port)
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int yes=1;
+    int rv;
+} 
+// This block of code looks at the local network interfaces and
+
+  // tries to find some that match our requirements (namely either
+
+  // IPv4 or IPv6 (AF_UNSPEC) and TCP (SOCK_STREAM) and use any IP on
+
+  // this machine (AI_PASSIVE).
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+
+  if ((rv = getaddrinfo(NULL, port, &hints, &servinfo))! = 0){
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+      return -1;
+  }
+  // Once we have a list of potential interfaces, loop through them
+
+  // and try to set up a socket on each. Quit looping the first time
+
+  // we have success.
+  for(p = servinfo; p != NULL; p = p->ai_next){
+      //try to make a socket based on this candidate interface
+
+      if ((sockfd = socket(p->ai_family, p->ai_socktype,
+      p->ai_protocol  ))== -1){
+          //perror("server:socket");
+          continue;
+      }
+      // SO_REUSEADDR prevents the "address already in use" errors
+
+    // that commonly come up when testing servers.
+
+     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR.))
+  }
+ 
+ 
