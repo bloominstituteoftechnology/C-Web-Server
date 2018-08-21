c3fa90d51eba2fc68ce0a6d5a5d518d8afacc5bb
maxwashington <mnwashington18@gmail.com>
2018-08-21 14:13:44 -0500

    Finished typing out solution code

--- src/servercopy.c
+++ src/servercopy.c
@@ -106,7 +106,8 @@ int get_listener_socket(char *port)
     return sockfd;
 }
 
-int send_responese(int fd, char *header, char *content_type, char body){
+int send_responese(int fd, char *header, char *content_type, char body)
+{
     const int max_response_size = 65536;
     char response[max_response_size];
 
@@ -115,26 +116,197 @@ int send_responese(int fd, char *header, char *content_type, char body){
 
     int content_length = strlen(body);
 
-    int response_length = sprintf(response,"%s\n"
-    "Date: %s" 
-    "Connection: close\n"
-    "Content-Length: %d\n"
-    "Content-Type: %s\n"
-    "\n" 
-    "%s\n",
-    
-    header,
-    asctime(ltime),
-    content_length,
-    content_type,
-    body
-    );
+    int response_length = sprintf(response, "%s\n"
+                                            "Date: %s"
+                                            "Connection: close\n"
+                                            "Content-Length: %d\n"
+                                            "Content-Type: %s\n"
+                                            "\n"
+                                            "%s\n",
+
+                                  header,
+                                  asctime(ltime),
+                                  content_length,
+                                  content_type,
+                                  body);
 
     int rv = send(fd, response, response_length, 0);
 
-    if(rv < 0){
+    if (rv < 0)
+    {
         perror("send");
     }
     return rv;
 }
 
+void resp_404(int fd, char *path)
+{
+    char response_body[1024];
+
+    sprintf(response_body, "404: %s not found", path);
+
+    send_responese(fd, "HTTP/1.1 404 NOT FOUND", "text/html", response_body);
+}
+
+void get_root(int fd)
+{
+    char *response_body = "<html><head></head><body><h1>Hello, World!</h1></body></html>\n";
+
+    send_responese(fd, "HTTP/1.1 200 OK", "text/html", response_body);
+}
+
+void get_d20(int fd)
+{
+    srand(time(NULL) + getpid());
+
+    char response_body[8];
+    sprintf(response_body, "%d\n", (rand() % 20) + 1);
+
+    send_responese(fd, "HTTP/1.1 200 OK", "text/plain", response_body);
+}
+
+void get_date(int fd)
+{
+    char response_body[128];
+    time_t t1 = time(NULL);
+    struct tm *gtime = gmtime(&t1);
+
+    sprintf(response_body, "%s", asctime(gtime));
+
+    send_responese(fd, "HTTP/1.1 200 OK", "text/plain", response_body);
+}
+
+void post_save(int fd, char *body)
+{
+    char *ststus;
+
+    int file_fd = open("data.txt", O_CREAT | O_WRONLY, 0644);
+
+    if (file_fd >= 0)
+    {
+        flock(file_fd, LOCK_EX);
+        write(file_fd, body, strlen(body));
+        flock(file_fd, LOCK_UN);
+        close(file_fd);
+
+        status = "ok";
+    }
+    else
+    {
+        status = "fail";
+    }
+    char response_body[128];
+
+    sprintf(response_body, "{\"status\": \"%s\"}\n", status);
+
+    send_responese(fd, "HTTP/1.1 200 OK", "application/json", response_body);
+}
+
+char *find_start_of_body(char *header)
+{
+    char *p;
+    p = strstr(header, "\n\n");
+    if (p != NULL)
+        return p;
+    p = strstr(header, "\r\n\r\n");
+    if (p != NULL)
+        return p;
+    p = strstr(header, "\r\r");
+
+    return p;
+}
+
+void handle_http_request(int fd)
+{
+    const int request_buffer_size = 65536;
+    char request[request_buffer_size];
+    char *p;
+    char request_type[8];
+    char request_type[1024];
+    char request_type[128];
+
+    int bytes_recvd = recv(fd, request, request_buffer_size -1, 0);
+
+    if (bytes_recvd < 0){
+        perror("recv");
+        return;
+    }
+    request[bytes_recvd] = "\0";
+
+    p = find_start_of_body(request);
+
+    if (p == NULL){
+        printf("Could not find end of header\n");
+        exit(1);
+    }
+    char *body = p;
+
+    printf("REQUEST: %s %s %s\n", request_type, request_path, request_protocol);
+
+  if (strcmp(request_type, "GET") == 0) {
+
+    if (strcmp(request_path, "/") == 0) {
+      get_root(fd);
+    }
+    else if (strcmp(request_path, "/d20") == 0) {
+      get_d20(fd);
+    }
+    else if (strcmp(request_path, "/date") == 0) {
+      get_date(fd);
+    }
+
+    else {
+      resp_404(fd, request_path);
+    }
+  }
+
+  else if (strcmp(request_type, "POST") == 0) {
+
+    if (strcmp(request_path, "/save") == 0) {
+      post_save(fd, body);
+
+    } else {
+      resp_404(fd, request_path);
+    }
+  }
+
+  else {
+    fprintf(stderr, "unknown request type \"%s\"\n", request_type);
+    return;
+  }
+}
+
+int main(void)
+{
+    int newfd;
+    struct sockaddr_storage their_addr;
+    char s[INET6_ADDRSTRLEN];
+
+    start_reaper();
+    int listenfd = get_listener_socket(PORT);
+
+    if(listenfd < 0){
+        fprintf(stderr, "webserver: fatal error getting listening socket\n");
+        exit(1);
+    }
+    printf("wenserver: waiting for connectins...\n");
+
+    while(1){
+        socklen_t sin_size = sizeof their_addr;
+        newfd = accept(listenfd, (struct sockaddr *)& their_addr, &sin_size);
+        if(newfd == -1){
+            perror("accept");
+            continue;
+        }
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        if(fork()== 0){
+            close(listenfd);
+            handle_http_request(newfd);
+            exit(0);
+        }
+        close(newfd);
+    }
+    return 0;
+}
