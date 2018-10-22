ea4f804fc7bcf40323e5c0161e06222914d1a37c
Kevin Tena <kevten09@gmail.com>
2018-10-22 17:54:15 -0500

    Implemented handle_http_request() in server.c

--- src/server.c
+++ src/server.c
@@ -144,6 +144,9 @@ void handle_http_request(int fd, struct cache *cache)
 {
     const int request_buffer_size = 65536; // 64K
     char request[request_buffer_size];
+    char request_type[4];
+    char request_path[128];
+    char request_http[128];
 
     // Read request
     int bytes_recvd = recv(fd, request, request_buffer_size - 1, 0);
@@ -160,11 +163,21 @@ void handle_http_request(int fd, struct cache *cache)
 
     // Read the three components of the first request line
 
+    sscanf(request, "%s %s %s\n", request_type, request_path, request_http);
+
     // If GET, handle the get endpoints
-
+    if(strcmp(request_type, "GET") == 0){
+    
     //    Check if it's /d20 and handle that special case
-    //    Otherwise serve the requested file by calling get_file()
+        if(strcmp(request_path, "/d20") == 0)
+            get_d20(fd);
 
+    //    Otherwise serve the requested file by calling get_file()
+        else
+            get_file(fd, cache, request_path);
+    }
+    else
+        resp_404(fd);
 
     // (Stretch) If POST, handle the post request
 }
