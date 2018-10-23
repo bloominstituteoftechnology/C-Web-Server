da01f9ef647a4f06b17944ea37bb7df84dd214be
-tristan- <tristan.linehan@gmail.com>
2018-10-23 07:22:01 -0700

    implemented: handle_request(), get_d20(), send_response(), and get_time()

--- src/server.c
+++ src/server.c
@@ -48,8 +48,44 @@
  * 
  * Return the value from the send() function.
  */
+/* EXAMPLE RESPONSE
+HTTP/1.1 200 OK
+Date: Wed Dec 20 13:05:11 PST 2017
+Connection: close
+Content-Length: 41749
+Content-Type: text/html
+
+<!DOCTYPE html><html><head><title>Lambda School ...
+
+*** The end of the header on both the request and response is marked by a blank line (i.e. two newlines in a row).
+*/
+void get_time(char *return_time)
+{
+    time_t current_time;
+    struct tm *local_time;
+    char *created_time; 
+
+    current_time = time(NULL);
+    local_time = localtime( &current_time );
+    // printf("%s\n", asctime (local_time));
+    created_time = asctime (local_time);
+    // printf("created_time: %s\n", created_time);
+    sprintf(return_time,"%s", created_time);
+    // printf("created_time after sprintf: %s\n", created_time);
+    *return_time = *created_time;
+    // NO NEED TO RETURN
+}
 int send_response(int fd, char *header, char *content_type, void *body, int content_length)
 {
+    /*
+        //  (void)header;
+        //  (void)content_type;
+        //  (void)body;
+        //  (void)content_length;
+        //  (void)fd;
+        //  (void)response;
+        //  (void)max_response_size;
+    */
     const int max_response_size = 65536;
     char response[max_response_size];
 
@@ -58,7 +94,15 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    char return_time[80];
+    get_time(return_time);
+    // printf("return_time from send():%s\n", return_time);
+    // printf("sizeof:%ld\n", sizeof(return_time));
+    int res_len = sprintf(response, "HTTP/1.1 %s\nDate:%sConnection: close\nContent-Length: %i\nContent-Type: %s\n\n%s\n\n", header, return_time, content_length, content_type, body);
+    // printf("dest:%s\n", body);
+    printf("  <-- <start of res>\n%s    <end of res>\n", response);
 
+    int response_length = res_len;
     // Send it all!
     int rv = send(fd, response, response_length, 0);
 
@@ -75,17 +119,37 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
  */
 void get_d20(int fd)
 {
+    // (void)fd;
     // Generate a random number between 1 and 20 inclusive
     
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    int random_num;
+    srand(time(NULL));
+    random_num = rand() % 20 +1;
+    // printf("%i\n", random_num);
+
+    // (void)random_num;
 
     // Use send_response() to send it back as text/plain data
 
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    // send_response(int fd, char *header, char *content_type, void *body, int content_length)
+    //send_response(fd, "HTTP/1.1 404 NOT FOUND", mime_type, filedata->data, filedata->size);
+// ^- REF ONLY
+    char body[2];    
+    int len_body = sprintf(body, "%i", random_num);
+    // printf("src:%i\n", random_num);
+    // printf("1:%i\n", ret_val);
+     // BELOW, SAME VALUE AS RETURNED FROM SPRINTF
+     // int ret_val_two = strlen(body);
+    // printf("2:%i\n", ret_val_two);
+    // printf("3:%i\n", len_body);
+
+    send_response(fd, "201 CREATED", "text/plain", body, len_body);
 }
 
 /**
@@ -119,6 +183,9 @@ void resp_404(int fd)
  */
 void get_file(int fd, struct cache *cache, char *request_path)
 {
+     (void)fd;
+     (void)cache;
+     (void)request_path;
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
@@ -132,9 +199,11 @@ void get_file(int fd, struct cache *cache, char *request_path)
  */
 char *find_start_of_body(char *header)
 {
+     (void)header;
     ///////////////////
     // IMPLEMENT ME! // (Stretch)
     ///////////////////
+     return(0);
 }
 
 /**
@@ -142,6 +211,7 @@ char *find_start_of_body(char *header)
  */
 void handle_http_request(int fd, struct cache *cache)
 {
+     (void)cache;
     const int request_buffer_size = 65536; // 64K
     char request[request_buffer_size];
 
@@ -157,14 +227,44 @@ void handle_http_request(int fd, struct cache *cache)
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+     // printf("--> incoming request:\n%s<end of req>\n",request);
+    // RETURNS:
+        // GET /test HTTP/1.1
+        // Host: localhost:3490
+        // User-Agent: curl/7.61.1
+        // Accept: */*
 
     // Read the three components of the first request line
+    char req_type[5];
+    char req_endpoint[20];
+    // SSCANF BREAKS DOWN THE REQ INTO VARs
+    sscanf(request, "%s %s", req_type, req_endpoint);
+    // printf("req_type: %s\nreq_endpoint: %s\n", req_type, req_endpoint);
+    printf("  --> incoming request: %s %s\n", req_type, req_endpoint);
+
+    if (strlen(req_endpoint) > 1) {
+        // MEMMOVE, REMOVES THE LEADING '/'
+        memmove(req_endpoint, req_endpoint+1, strlen(req_endpoint));
+        // printf("NEW req_endpoint: %s\n", req_endpoint);
+    }
+    // printf("NEW req_endpoint: %s\n", req_endpoint);
 
     // If GET, handle the get endpoints
 
+    if (strcmp(req_type, "GET") == 0) {
+        // printf("found GET in req\n");
+        if (strcmp(req_endpoint, "/") == 0) {
+            printf("found HOME\n");
+            return;
+        }
     //    Check if it's /d20 and handle that special case
+        else if (strcmp(req_endpoint, "d20") == 0) {
+            // printf("found d20 in endpoint\n");
+            get_d20(fd);
+        }
     //    Otherwise serve the requested file by calling get_file()
 
+    }
 
     // (Stretch) If POST, handle the post request
 }
