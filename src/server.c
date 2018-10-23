4a0b88e1e17a5b7b5648cced698146cd2cc64a00
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-10-23 16:42:45 -0500

    fix randomizer

--- src/server.c
+++ src/server.c
@@ -78,11 +78,7 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
-    int d20;
-    time_t t;
-
-    srand((unsigned)time(&t));
-    d20 = rand() % 21;
+    srand(getpid() + time(NULL));
 
     ///////////////////
     // IMPLEMENT ME! //
@@ -91,7 +87,7 @@ void get_d20(int fd)
     // Use send_response() to send it back as text/plain data
     char response_body[8];
 
-    sprintf(response_body, "%d\n", d20);
+    sprintf(response_body, "%d\n", rand() % 21);
     send_response(fd, "HTTP/1.1 200 OK", "text/plain", response_body, strlen(response_body));
 
     ///////////////////
