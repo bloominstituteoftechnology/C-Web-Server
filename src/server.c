fc0e93c2692e94d18400b19977e56baa37d0ff2c
Lonnie Davis <ldavis0866@gmail.com>
2018-10-23 18:13:12 -0500

    send request and get file complete

--- src/server.c
+++ src/server.c
@@ -53,12 +53,17 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
     const int max_response_size = 65536;
     char response[max_response_size];
 
-    // Build HTTP response and store it in response
+    time_t t = time(NULL);
+    struct tm *gm = gmtime(&t); // 
 
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
-    int response_length = 0;
+
+
+    int response_length = sprintf(response, "%s\n" "Date: %s" "Connection: close\n" "Content-Lenght: %d\n" "Content-Type: %s" "\n" "%s\n", header, asctime(gm), content_length, content_type, body);
+
+    
+
+
+    
     // Send it all!
     int rv = send(fd, response, response_length, 0);
 
@@ -116,9 +121,31 @@ void resp_404(int fd)
  */
 void get_file(int fd, struct cache *cache, char *request_path)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    char filepath[4096];
+
+    struct file_data *filedata;
+    char *mime_type;
+
+    snprintf(filepath, sizeof(filepath),"%s%s", SERVER_ROOT, request_path);
+
+    filedata = file_load(filepath);
+
+    if (filedata == NULL) {
+        snprintf(filepath, sizeof(filepath),"%s%s/index.html", SERVER_ROOT);
+        filedata = file_load(filepath);
+
+        if (filedata == NULL) {
+            resp_404(fd);
+            return;
+        }
+        
+    }
+
+    mime_type = mime_type_get(filepath);
+
+    send_response(fd, "HTTP/1.1 200 OK", mime_type, filedata->data, filedata->size);
+
+    file_free(filedata);
 }
 
 /**
