9866852212ded4396a50875d525ba317636c2d59
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-10-25 12:46:24 -0700

    caching works properly

--- src/cache.c
+++ src/cache.c
@@ -12,11 +12,20 @@ struct cache_entry *alloc_entry(char *path, char *content_type, void *content, i
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
-    struct cache_entry *ce = malloc(sizeof(struct cache_entry));
-    ce->path = path;
+    struct cache_entry *ce = malloc(sizeof *ce);
+
+    // Set the fields in the new cache entry
+    ce->path = malloc(strlen(path) + 1);
+    strcpy(ce->path, path);
+
+    ce->content_type = malloc(strlen(content_type) + 1);
+    strcpy(ce->content_type, content_type);
+
     ce->content_length = content_length;
-    ce->content_type = content_type;
-    ce->content = content;
+
+    ce->content = malloc(content_length);
+    memcpy(ce->content, content, content_length);
+
     return ce;
 }
 
@@ -104,6 +113,14 @@ struct cache *cache_create(int max_size, int hashsize)
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    struct hashtable *hash = hashtable_create(hashsize, NULL);
+    struct cache *cache_inst = malloc(sizeof(struct cache));
+    cache_inst->head = NULL;
+    cache_inst->tail = NULL;
+    cache_inst->cur_size = 0;
+    cache_inst->max_size = max_size;
+    cache_inst->index = hash;
+    return cache_inst;
 }
 
 /**
@@ -118,6 +135,19 @@ void cache_put(struct cache *cache, char *path, char *content_type, void *conten
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    struct cache_entry *ce = alloc_entry(path, content_type, content, content_length);
+    dllist_insert_head(cache, ce);
+    hashtable_put(cache->index, ce->path, ce);
+    cache->cur_size++;
+    if (cache->cur_size > cache->max_size)
+    {
+        hashtable_delete(cache->index, cache->tail->path);
+        free_entry(dllist_remove_tail(cache));
+        if (cache->cur_size > cache->max_size)
+        {
+            printf("Capacity is not enough!");
+        }
+    }
 }
 
 /**
@@ -128,4 +158,32 @@ struct cache_entry *cache_get(struct cache *cache, char *path)
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    struct cache_entry *ce;
+
+    ce = hashtable_get(cache->index, path);
+
+    if (ce == NULL)
+    {
+        return NULL;
+    }
+    dllist_move_to_head(cache, ce);
+
+    return ce;
 }
+
+void cache_free(struct cache *cache)
+{
+    struct cache_entry *cur_entry = cache->head;
+
+    hashtable_destroy(cache->index);
+
+    while (cur_entry != NULL)
+    {
+        struct cache_entry *next_entry = cur_entry->next;
+
+        free_entry(cur_entry);
+
+        cur_entry = next_entry;
+    }
+}
+
