cb560fba460ebafac4ff437e78645b746f91c5a2
Munsu <dark_specineff2002@yahoo.com>
2018-12-03 12:51:06 -0800

    initial PR

--- src/server.c
+++ src/server.c
@@ -55,6 +55,8 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 
     // Build HTTP response and store it in response
 
+
+
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
