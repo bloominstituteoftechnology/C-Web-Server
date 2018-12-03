27d078a784bf7282909c9bdead26234b6f153575
codejoncode <jonathanjholloway@gmail.com>
2018-12-03 15:59:20 -0600

    trying to get the  server send_response going

--- src/server.c
+++ src/server.c
@@ -58,6 +58,9 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    sprintf(response, "%s\n%s\n%s\n\n", header, content_type, body);
+    int response_length = strlen(response);
+
 
     // Send it all!
     int rv = send(fd, response, response_length, 0);
