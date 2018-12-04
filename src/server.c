07f5a0fd67bb97e93ac1c661ceab108f420b2e07
sca4cs <shannon.atkinson.ca@gmail.com>
2018-12-04 11:38:13 -0500

    get_d20 working

--- src/server.c
+++ src/server.c
@@ -80,17 +80,16 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
-    int upper = 20;
-    int lower = 0;
-    time_t t = time(0);
 
-    srand(t);
-
-    int num = (rand() % (upper - lower + 1)) + lower; 
+    srand(time(NULL));
+    char body[3];
+    int num = (rand() % 20) + 1;
+    int length = sprintf(body, "%d", num); 
 
     // Use send_response() to send it back as text/plain data
 
-    send_response(fd, "HTTP/1.1 200 OK", "text/plain", num, 4);
+    send_response(fd, "HTTP/1.1 200 OK", "text/plain", body, length);
+
 }
 
 /**
