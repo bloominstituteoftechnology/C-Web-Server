8c393eda960155f516b07a833db838220d3700f4
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-12-04 10:05:02 -0800

    d20 working. Working on get_file() method.

--- src/server.c
+++ src/server.c
@@ -52,10 +52,17 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 {
     const int max_response_size = 65536;
     char response[max_response_size];
-    int response_length;
 
     // Build HTTP response and store it in response
-    response_length = sprintf(response, "%s\n Connection: close\n  Content-Length: %d\n Content-Type: %s\n\n %s\n", header, content_length, content_type, body);
+    time_t rawtime;
+    struct tm *info;
+
+    time(&rawtime);
+    info = localtime(&rawtime);
+
+    int response_length = sprintf(response, "%s\n Connection: close\nContent-Length: %d\nContent-Type: %s\nDate: %s\n%s\n ", header, content_length, content_type, asctime(info), body);
+    // Connection: close\n  Content-Length: %d\n Content-Type: %s\n\n %s\n"
+    // content_length, content_type, body
 
     // Send it all!
     int rv = send(fd, response, response_length, 0);
@@ -78,12 +85,16 @@ void get_d20(int fd)
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    char number[10];
+    int randomNumber = rand() % 21;
+    int len = sprintf(number, "%d\n", randomNumber);
 
     // Use send_response() to send it back as text/plain data
 
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    send_response(fd, "HTTP/1.1 200 OK", "text/plain", number, len);
 }
 
 /**
@@ -157,9 +168,21 @@ void handle_http_request(int fd, struct cache *cache)
     ///////////////////
 
     // Read the three components of the first request line
+    char method[5];
+    char file[20];
+    char protocol[20];
+
+    sscanf(request, "%s %s %s", method, file, protocol);
+    printf("Method: %s\n", method);
 
     // If GET, handle the get endpoints
-
+    if (strcmp(method, "GET") == 0) {
+        if (strcmp(file, "/d20") == 0) {
+            get_d20(fd);
+        } else {
+            resp_404(fd);
+        }
+    }
     //    Check if it's /d20 and handle that special case
     //    Otherwise serve the requested file by calling get_file()
 
