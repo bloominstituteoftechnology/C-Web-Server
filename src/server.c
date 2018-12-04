8441c162be0c007b00107ba4f7ee094ae597cc51
meltzerkyle <meltzerkyle@gmail.com>
2018-12-04 16:58:09 -0600

    completed get_file

--- src/server.c
+++ src/server.c
@@ -123,9 +123,22 @@ void resp_404(int fd)
  */
 void get_file(int fd, struct cache *cache, char *request_path)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    char filepath[4096];
+    struct file_data *filedata;
+    char *mime_type;
+
+    // Fetch the 404.html file
+    snprintf(filepath, sizeof filepath, "%s/%s", SERVER_ROOT, request_path);
+    filedata = file_load(filepath);
+
+    if (filedata == NULL)
+    {
+        resp_404(fd);
+    }
+
+    mime_type = mime_type_get(filepath);
+    send_response(fd, "HTTP/1.1 200 OK", mime_type, filedata->data, filedata->size);
+    file_free(filedata);
 }
 
 /**
@@ -185,6 +198,7 @@ void handle_http_request(int fd, struct cache *cache)
         }
         else
         {
+            //add if statement here somewhere to check for valid path
             get_file(fd, cache, request_path);
         }
     }
