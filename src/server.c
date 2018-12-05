c11140ff98a08d812f633dcdd4cbc2c72a3679ac
fron12 <sohnf90@yahoo.com>
2018-12-05 13:29:50 -0800

    Implemented arbitrart file serving

--- src/server.c
+++ src/server.c
@@ -141,6 +141,24 @@ void get_file(int fd, struct cache *cache, char *request_path)
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    char filepath[4096];
+    struct file_data *filedata; 
+    char * mime_type;
+
+    sprintf(filepath, "%s%s", SERVER_ROOT, request_path);
+
+    filedata = file_load(filepath);
+
+    if (filedata == NULL) {
+        resp_404(fd);
+    }
+    else {
+        mime_type = mime_type_get(filepath);
+
+        send_response(fd, "HTTP/1.1 200 OK", mime_type, filedata->data,filedata->size);
+    }
+
+    file_free(filedata);
 }
 
 /**
