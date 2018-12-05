a3da1e49fade14c95f7d6f732abd54ef64e7a38b
William <kwontaewoongah@gmail.com>
2018-12-04 20:03:59 -0800

    modified cache delete function to account for head and tail

--- src/cache.c
+++ src/cache.c
@@ -176,8 +176,10 @@ void cache_delete(struct cache *cache,char *path) {
     struct cache_entry *old_item=hashtable_delete(cache->index,path);
     if (old_item->next==NULL && old_item->prev) {
         old_item->prev->next=NULL;
+        cache->tail=old_item->prev;
     } else if (old_item->prev==NULL && old_item->next) {
         old_item->next->prev=NULL;
+        cache->head=old_item->next;
     } else {
         old_item->next->prev=old_item->prev;
         old_item->prev->next=old_item->next;
