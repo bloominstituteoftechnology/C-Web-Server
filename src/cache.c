0575ebf38849f7b6ebc6819ee1d9adf29c5c4ad6
Charlie Winslow <cwinslow22@yahoo.com>
2018-12-05 22:49:41 -0600

    first attempt at cache_put implementation

--- src/cache.c
+++ src/cache.c
@@ -150,9 +150,29 @@ void cache_free(struct cache *cache)
  */
 void cache_put(struct cache *cache, char *path, char *content_type, void *content, int content_length)
 {
-    ///////////////////
     // IMPLEMENT ME! //
-    ///////////////////
+    // Allocate a new cache entry with the passed parameters
+    struct cache_entry *nce = alloc_entry(path, content_type, content, content_length);
+    //Insert the entry at the head of the doubly-linked list
+    dllist_insert_head(cache, nce);
+    // Store the entry in the hashtable as well, indexed by the entry's path
+    hashtable_put(cache->index, path, nce);
+    // Increment the current size of the cache
+    cache->cur_size++;
+    // If the cache size is greater than the max size:
+    if (cache->cur_size > cache->max_size) {
+        // Remove the entry from the hashtable, using the entry's path and the hashtable_delete function
+        hashtable_delete(cache->index, cache->tail->path);
+        // Remove the cache entry at the tail of the linked list (this is the least-recently used one)
+        dllist_remove_tail(cache);
+        // Free the cache entry
+        free_entry(oldtail);
+        // Ensure the size counter for the number of entries in the cache is correct.
+        cache->cur_size--;
+    }
+
+
+
 }
 
 /**
