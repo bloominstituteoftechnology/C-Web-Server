68b9febb225432e3540ead8d86003892479325c2
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-12-07 09:49:55 -0500

    updated alloc test, test still passes

--- src/cache_tests/cache_tests.c
+++ src/cache_tests/cache_tests.c
@@ -30,14 +30,15 @@ char *test_cache_alloc_entry()
   char *path = "/bazz/lurman.html";
   char *content_type = "text/html";
   char *content = "<head>Bazz Lurman</head>";
+  int content_len = strlen(content) + 1; // +1 to include the \0
 
-  struct cache_entry *ce = alloc_entry(path, content_type, content, strlen(content));
+  struct cache_entry *ce = alloc_entry(path, content_type, content, content_len);
 
   // Check that the allocated entry was initialized with expected values
   mu_assert(check_strings(ce->path, path) == 0, "Your alloc_entry function did not allocate the path field to the expected string");
   mu_assert(check_strings(ce->content_type, content_type) == 0, "Your alloc_entry function did not allocate the content_type field to the expected string");
   mu_assert(check_strings(ce->content, content) == 0, "Your alloc_entry function did not allocate the content field to the expected string");
-  mu_assert(ce->content_length == strlen(content), "Your alloc_entry function did not allocate the content_length field to the expected length");
+  mu_assert(ce->content_length == content_len, "Your alloc_entry function did not allocate the content_length field to the expected length");
 
   free_entry(ce);
 
@@ -81,7 +82,7 @@ char *test_cache_put()
   mu_assert(check_cache_entries(cache->head->next->prev, test_entry_3) == 0, "Your cache_put function did not update the head->next->prev pointer to point to the new head entry");
   mu_assert(check_cache_entries(cache->head->next->next, test_entry_1) == 0, "Your cache_put function did not update the head->next->next pointer to point to the tail entry");
   mu_assert(check_cache_entries(cache->tail->prev, test_entry_2) == 0, "Your cache_put function did not update the tail->prev pointer to poin to the second-to-last entry");
-  mu_assert(check_cache_entries(cache->tail, test_entry_1) == 0, "Your cache_put function did not correctly update the tail pointer of the cache"); 
+  mu_assert(check_cache_entries(cache->tail, test_entry_1) == 0, "Your cache_put function did not correctly update the tail pointer of the cache");
 
   // Add in a fourth entry to the cache
   cache_put(cache, test_entry_4->path, test_entry_4->content_type, test_entry_4->content, test_entry_4->content_length);
