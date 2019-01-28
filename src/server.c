1a6841334ffd68539684a5af4584f3262a8f6fba
Nicocchi <nicocchilover@gmail.com>
2019-01-28 15:49:06 -0500

    send_response and handle_htt_request implemented.

--- src/server.c
+++ src/server.c
@@ -1,18 +1,18 @@
 /**
  * webserver.c -- A webserver written in C
- * 
+ *
  * Test with curl (if you don't have it, install it):
- * 
+ *
  *    curl -D - http://localhost:3490/
  *    curl -D - http://localhost:3490/d20
  *    curl -D - http://localhost:3490/date
- * 
+ *
  * You can also test the above URLs in your browser! They should work!
- * 
+ *
  * Posting Data:
- * 
+ *
  *    curl -D - -X POST -H 'Content-Type: text/plain' -d 'Hello, sample data!' http://localhost:3490/save
- * 
+ *
  * (Posting data is harder to test from a browser.)
  */
 
@@ -45,13 +45,22 @@
  * header:       "HTTP/1.1 404 NOT FOUND" or "HTTP/1.1 200 OK", etc.
  * content_type: "text/plain", etc.
  * body:         the data to send.
- * 
+ *
  * Return the value from the send() function.
  */
 int send_response(int fd, char *header, char *content_type, void *body, int content_length)
 {
     const int max_response_size = 65536;
     char response[max_response_size];
+
+    char t_buff[80];
+    time_t c_time = time(NULL);
+
+    // Format time
+    strftime(t_buff, (sizeof t_buff) -1, "%c", &c_time);
+
+    // Formate HTTP response
+    int response_length = sprintf( response, "%s\nDate: %s\nConnection: close\nContent-Length: %d\nContent-Type: %s\n\n%s", header, t_buff, content_length, content_type, body);
 
     // Build HTTP response and store it in response
 
@@ -76,7 +85,7 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
-    
+
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
@@ -94,7 +103,7 @@ void get_d20(int fd)
 void resp_404(int fd)
 {
     char filepath[4096];
-    struct file_data *filedata; 
+    struct file_data *filedata;
     char *mime_type;
 
     // Fetch the 404.html file
@@ -126,7 +135,7 @@ void get_file(int fd, struct cache *cache, char *request_path)
 
 /**
  * Search for the end of the HTTP header
- * 
+ *
  * "Newlines" in HTTP can be \r\n (carriage return followed by newline) or \n
  * (newline) or \r (carriage return).
  */
@@ -142,8 +151,12 @@ char *find_start_of_body(char *header)
  */
 void handle_http_request(int fd, struct cache *cache)
 {
+    printf("HANLDE HTTP REQUEST\n");
     const int request_buffer_size = 65536; // 64K
     char request[request_buffer_size];
+    char method[3];
+    char path[256];
+    char protocol[128];
 
     // Read request
     int bytes_recvd = recv(fd, request, request_buffer_size - 1, 0);
@@ -159,11 +172,24 @@ void handle_http_request(int fd, struct cache *cache)
     ///////////////////
 
     // Read the three components of the first request line
+    sscanf(request, "%s %s %s", method, path, protocol);
+
+    printf("METHOD -> %s\n", method);
+    printf("PATH -> %s\n", path);
+    printf("PROTOCOL -> %s\n", protocol);
 
     // If GET, handle the get endpoints
-
-    //    Check if it's /d20 and handle that special case
-    //    Otherwise serve the requested file by calling get_file()
+    if (strcmp(method, "GET")) {
+      //    Check if it's /d20 and handle that special case
+      printf("GET\n");
+      if (!strcmp(path, "/d20")) {
+        printf("GET /d20\n");
+        get_d20(fd);
+      } else {
+        //    Otherwise serve the requested file by calling get_file()
+        get_file(fd, cache, path);
+      }
+    }
 
 
     // (Stretch) If POST, handle the post request
@@ -193,7 +219,7 @@ int main(void)
     // This is the main loop that accepts incoming connections and
     // forks a handler process to take care of it. The main parent
     // process then goes back to waiting for new connections.
-    
+
     while(1) {
         socklen_t sin_size = sizeof their_addr;
 
@@ -210,7 +236,7 @@ int main(void)
             get_in_addr((struct sockaddr *)&their_addr),
             s, sizeof s);
         printf("server: got connection from %s\n", s);
-        
+
         // newfd is a new socket descriptor for the new connection.
         // listenfd is still listening for new connections.
 
@@ -223,4 +249,3 @@ int main(void)
 
     return 0;
 }
-
