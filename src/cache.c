6840da94db7b2bed94d5f808408055ae6a6a02fe
Carlos Vargas <cornielleandres@gmail.com>
2019-01-29 16:59:43 -0500

    Implement cache_create().

--- src/cache.c
+++ src/cache.c
@@ -91,9 +91,14 @@ struct cache_entry *dllist_remove_tail(struct cache *cache)
  */
 struct cache *cache_create(int max_size, int hashsize)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    struct cache *new_cache = malloc(sizeof(struct cache));
+    new_cache->index = hashtable_create(hashsize, NULL);
+    new_cache->head = NULL;
+    new_cache->tail = NULL;
+    new_cache->max_size = max_size;
+    new_cache->cur_size = 0;
+
+    return new_cache;
 }
 
 void cache_free(struct cache *cache)
