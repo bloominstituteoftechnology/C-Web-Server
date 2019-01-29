aefc4c81e4f86d1182c0004da61116499c8b255f
jlaluces123 <jlalucescareer123@gmail.com>
2019-01-29 15:57:07 -0800

    Day 2 - Part 4: Done

--- src/server.c
+++ src/server.c
@@ -58,12 +58,27 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
     time(&rawtime);
     info = localtime(&rawtime);
 
-    // Build HTTP response and store it in response
-    int response_length = sprintf(response, "%s\nDate: %s\nConnection: close\nContent-length: %d\nContent-type: %s\n%s\n", header, asctime(info), content_length, content_type, body);
-    printf("response: %s\n============================", response); // --> We can use a sprintf() to create the response, also returns total number of bytes in result string
+    // Build HTTP response and store it in response || double \n here because it'll mess up the HTML loading
+    // int response_length = sprintf(response, "%s\nDate: %s\nConnection: close\nContent-length: %d\nContent-type: %s\n\n%s\n", header, asctime(info), content_length, content_type, body);
+    int response_length = sprintf(response,
+        "%s\n"
+        "Date: %s"
+        "Content-Length: %d\n"
+        "Content-Type: %s\n"
+        "Connection: close\n"
+        "\n"
+        "%s\n",
+        header,
+        asctime(info),
+        content_length,
+        content_type,
+        body
+    );
+    printf("response: %s\n", response); // --> We can use a sprintf() to create the response, also returns total number of bytes in result string
+    // memcpy(response + response_length, body, content_length);
     ///////////////////
     // IMPLEMENT ME! //
-    ///////////////////
+    ///////////////////    
 
     // Send it all!
     int rv = send(fd, response, response_length, 0);
@@ -86,7 +101,7 @@ void get_d20(int fd)
     srand( time(0) );
 
     // 2. Make the random number
-    int random_number = ( rand() % 20 ) + 1;
+    int random_number = ( rand() % 20 );
     printf("random number is: %d\n", random_number);
 
     // 3. Assign the number to the data given into send_response 3rd arg
@@ -138,9 +153,28 @@ void resp_404(int fd)
  */
 void get_file(int fd, struct cache *cache, char *request_path)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    // 1. Create a file_path buffer & initialize a file_data struct
+    char file_path[4096];
+    struct file_data *filedata; 
+    char *mime_type;  
+
+    // 2. Handle whether or not the request path is empty (aka the home page) or snprintf() the another file path
+    if ( strcmp(request_path, "/") == 0 ) {        
+        snprintf(file_path, sizeof file_path, "%s%s", SERVER_ROOT, "/index.html");
+    } else {
+        snprintf(file_path, sizeof file_path, "%s%s", SERVER_ROOT, request_path);
+    }
+    // 3. Load file data
+    filedata = file_load(file_path);
+
+    // 4. Check if file_load() encountered an error, send response to server and free file
+    if (filedata == NULL) {
+        resp_404(fd);
+    } else {
+        mime_type = mime_type_get(file_path);
+        send_response(fd, "HTTP/1.1 200 OK", mime_type, filedata->data, filedata->size);
+        file_free(filedata);
+    }
 }
 
 /**
@@ -198,16 +232,16 @@ void handle_http_request(int fd, struct cache *cache)
     // If GET, handle the get endpoints
     if(!strcmp(request_type, "GET")) {
         // Check if it's /d20 and handle that special case
-        printf("strcmp GET print check\n");
+        // printf("strcmp GET print check\n");
         if (!strcmp(request_endpoint, "/d20")) {
-            printf("Request-endpoint: /d20\n");
+            // printf("Request-endpoint: /d20\n");
             get_d20(fd);
         } else {
             // Otherwise serve the requested file by calling get_file()
             /*
                 - int fd, struct cache *cache, char *request_path
             */
-            printf("Serve requested file (get_file)\n");
+            // printf("Serve requested file (get_file)\n");
             get_file(fd, cache, request_endpoint);
         }
     }
