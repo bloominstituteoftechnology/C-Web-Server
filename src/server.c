5297645ea47fa9cd743fd655910a9271894b66a9
Sean Chen <skypemaster007@gmail.com>
2019-01-31 08:59:01 -0800

    Update server.c

--- src/server.c
+++ src/server.c
@@ -50,7 +50,7 @@
  */
 int send_response(int fd, char *header, char *content_type, void *body, int content_length)
 {
-    const int max_response_size = 65536;
+    const int max_response_size = 262144;
     char response[max_response_size];
 
     // Build HTTP response and store it in response
