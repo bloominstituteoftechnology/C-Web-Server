9189445a6aa92c6dd9618b4830178a4f1aa9bc25
verydecent <wnjhwng@gmail.com>
2019-02-04 12:42:13 -0800

    working server

--- src/server.c
+++ src/server.c
@@ -146,7 +146,7 @@ void get_file(int fd, struct cache *cache, char *request_fpath)
     snprintf(path, "%s%s", SERVER_ROOT, request_fpath);
 
     if (check_cache !=NULL) {
-        send_response(fd, "HTTP/1.1 200 OK", check_cache->content_type, 
+        send_respon se(fd, "HTTP/1.1 200 OK", check_cache->content_type, 
         check_cache->content, check_cache->content_length);
     } else{
     filedata = file_load(path);
