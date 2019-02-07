0b5b7441c0c05f74cba57c480994f45bafb2d36a
Linda Yang <lindayang@Lindas-iMac.fios-router.home>
2019-02-06 19:11:14 -0500

    Added algorithm comments

--- src/cache.c
+++ src/cache.c
@@ -126,19 +126,28 @@ void cache_put(struct cache *cache, char *path, char *content_type, void *conten
     // IMPLEMENT ME! //
     ///////////////////
 
+    // Allocate a new cache entry with the passed parameters
     struct cache_entry *new = alloc_entry(path, content_type, content, content_length);
     
+    // Insert the entry at the head of the doubly-linked list
     dllist_insert_head(cache, new);
 
+    // Store the entry in the hastable as well, indexed by the entry's path
     hashtable_put(cache->index, path, new);
     
+    // Increment the current size of the cache
     cache->cur_size++;
 
+    // If the cache size is greater than the max size:
     if (cache->cur_size > cache->max_size) {
+        // Remove the cache entry at the tail of the linked list
         struct cache_entry *last_tail = dllist_remove_tail(cache);
+        // Remove that same entry from the hashtable, using the entry's path and the hashtable_delete function
         hashtable_delete(cache->index, last_tail->path);
+        // Free the cache entry
         free_entry(last_tail);
 
+        // Ensure the size counter for the number of entries in the cache is correct
         if (cache->cur_size > cache->max_size) {
             cache->cur_size--;
         }
