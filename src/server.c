0f7e1ba407a499ffb1aa6d904bc3f4295adbf826
Conner Alegre <cga0@pm.me>
2019-03-04 15:35:43 -0700

    Initial commit

--- src/server.c
+++ src/server.c
@@ -55,10 +55,6 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 
     // Build HTTP response and store it in response
 
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
-
     // Send it all!
     int rv = send(fd, response, response_length, 0);
 
@@ -152,6 +148,8 @@ void handle_http_request(int fd, struct cache *cache)
         perror("recv");
         return;
     }
+
+    printf("REQUEST:::: %d\n", request);
 
 
     ///////////////////
