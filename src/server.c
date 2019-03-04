f1170d38b1f30b1fcb206d3dd3ca52fb891f116b
ckirkwood94 <ckirkwood94@gmail.com>
2019-03-04 18:50:34 -0500

    Implemented get_d20

--- src/server.c
+++ src/server.c
@@ -94,16 +94,12 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
-
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
-
+    srand(time(NULL));
+    int random_num = (rand() % 20) + 1;
+    char response_body[8];
+    sprintf(response_body, "%d\n", random_num);
     // Use send_response() to send it back as text/plain data
-
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    send_response(fd, "HTTP/1.1 200 OK", "text/plain", response_body, strlen(response_body));
 }
 
 /**
