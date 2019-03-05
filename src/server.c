0ddcadd5962110e92eafb850f31127a924792765
Max <maxkajiwara@gmail.com>
2019-03-05 13:56:36 -0800

    Implement get_d20()

--- src/server.c
+++ src/server.c
@@ -84,16 +84,10 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
-
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    int roll = rand() % (20) + 1;
 
     // Use send_response() to send it back as text/plain data
-
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    send_response(fd, "HTTP/1.1 200 OK", "text/plain", roll, 1);
 }
 
 /**
