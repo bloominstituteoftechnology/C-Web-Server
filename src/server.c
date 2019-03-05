fd69d4455aa689add4ca84e71cb46d1e7cbb25cc
Linh Bouniol <paulalinh89@gmail.com>
2019-03-05 14:02:46 -0800

    Implemented send_response

--- src/server.c
+++ src/server.c
@@ -55,9 +55,19 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 
     // Build HTTP response and store it in response
 
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    // Build the header
+    sprintf(response, max_response_size, 
+        "%s\n"
+        "Connection: close\n"
+        "Content-Length: %d\n"
+        "Content-Type: %s\n"
+        "\n"
+        "%s",
+        
+        header, content_length, content_type, body
+    );
+    
+    response_length = strlen(response);
 
     // Send it all!
     int rv = send(fd, response, response_length, 0);
