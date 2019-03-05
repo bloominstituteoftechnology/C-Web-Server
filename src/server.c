d319e546e6892c21d4fc01a2be844c07c420bfc9
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-03-05 18:47:14 -0500

    handle http request

--- src/server.c
+++ src/server.c
@@ -65,6 +65,7 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
                       "\n"
                       "%s",
             header, content_type, content_length, body);
+    int response_length = strlen(response);
 
     // Send it all!
     int rv = send(fd, response, response_length, 0);
@@ -152,6 +153,8 @@ void handle_http_request(int fd, struct cache *cache)
 {
     const int request_buffer_size = 65536; // 64K
     char request[request_buffer_size];
+    char method[512];
+    char path[8192];
 
     // Read request
     int bytes_recvd = recv(fd, request, request_buffer_size - 1, 0);
@@ -167,9 +170,21 @@ void handle_http_request(int fd, struct cache *cache)
     ///////////////////
 
     // Read the three components of the first request line
+    sscanf(request, "%s %s", method, path);
+    printf("GOT REQUEST: \"%s\" \"%s\" \n", method, path);
 
     // If GET, handle the get endpoints
-
+    if (strcmp(method, "/GET") == 0)
+    {
+        if (strcmp(path, "/monkey") == 0)
+        {
+            send_response(fd, "HTTP/1.1 200 OK", "text/plain", "monkey", 6);
+        }
+        else
+        {
+            resp_404(fd);
+        }
+    }
     //    Check if it's /d20 and handle that special case
     //    Otherwise serve the requested file by calling get_file()
 
