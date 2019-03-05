f0039189239627da2ebfc6ca7085962a4208ac28
Steven O'Neill <oneillsteven957@gmail.com>
2019-03-05 16:36:46 -0600

    implemented get file

--- src/server.c
+++ src/server.c
@@ -140,6 +140,25 @@ void get_file(int fd, struct cache *cache, char *request_path)
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+    char filepath[4096];
+    struct file_data *filedata; 
+    char *mime_type;
+
+    // Fetch the 404.html file
+    snprintf(filepath, sizeof filepath, "%s/%s", SERVER_ROOT, request_path);
+    filedata = file_load(filepath);
+
+    if (filedata == NULL) {
+        // TODO: make this non-fatal
+        resp_404(fd);
+        return;
+    }
+
+    mime_type = mime_type_get(filepath);
+
+    send_response(fd, "HTTP/1.1 200 OK", mime_type, filedata -> data, filedata -> size);
+
+    file_free(filedata);
 }
 
 /**
