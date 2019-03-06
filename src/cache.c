681329fcdf17581ea661b1bd009eda4505eab314
Andrew <ghobrial.andrew@gmail.com>
2019-03-06 18:05:52 -0500

    cache progress

--- src/cache.c
+++ src/cache.c
@@ -125,6 +125,29 @@ void cache_put(struct cache *cache, char *path, char *content_type, void *conten
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+
+    
+    struct cache_entry *new = alloc_entry(path, content_type, content, content_length);
+    
+    dllist_insert_head(cache, new);
+    
+    hashtable_put(cache->index, path, new);
+    
+    cache->cur_size++;
+    if (cache->cur_size > cache->max_size)
+    {
+        
+        struct cache_entry *last_tail = dllist_remove_tail(cache);
+        
+        hashtable_delete(cache->index, last_tail->path);
+        
+        free_entry(last_tail);
+        
+        if (cache->cur_size > cache->max_size)
+        {
+            cache->cur_size--;
+        }
+    }
 }
 
 /**
@@ -135,4 +158,14 @@ struct cache_entry *cache_get(struct cache *cache, char *path)
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
+
+
+    struct cache_entry *entry = hashtable_get(cache->index, path);
+
+    if (entry == NULL)
+    {
+        return NULL;
+    }
+    dllist_move_to_head(cache, entry);
+    return entry;
 }
