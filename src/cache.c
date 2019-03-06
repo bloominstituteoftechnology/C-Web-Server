331b94c5495d859d85acbfc7ff657e7d6b8ac8cd
Eva <evalee904@gmail.com>
2019-03-06 14:48:42 -0600

    cache_put

--- src/cache.c
+++ src/cache.c
@@ -30,10 +30,13 @@ void free_entry(struct cache_entry *entry)
 void dllist_insert_head(struct cache *cache, struct cache_entry *ce)
 {
     // Insert at the head of the list
-    if (cache->head == NULL) {
+    if (cache->head == NULL)
+    {
         cache->head = cache->tail = ce;
         ce->prev = ce->next = NULL;
-    } else {
+    }
+    else
+    {
         cache->head->prev = ce;
         ce->next = cache->head;
         ce->prev = NULL;
@@ -46,13 +49,16 @@ void dllist_insert_head(struct cache *cache, struct cache_entry *ce)
  */
 void dllist_move_to_head(struct cache *cache, struct cache_entry *ce)
 {
-    if (ce != cache->head) {
-        if (ce == cache->tail) {
+    if (ce != cache->head)
+    {
+        if (ce == cache->tail)
+        {
             // We're the tail
             cache->tail = ce->prev;
             cache->tail->next = NULL;
-
-        } else {
+        }
+        else
+        {
             // We're neither the head nor the tail
             ce->prev->next = ce->next;
             ce->next->prev = ce->prev;
@@ -64,7 +70,6 @@ void dllist_move_to_head(struct cache *cache, struct cache_entry *ce)
         cache->head = ce;
     }
 }
-
 
 /**
  * Removes the tail from the list and returns it
@@ -102,7 +107,8 @@ void cache_free(struct cache *cache)
 
     hashtable_destroy(cache->index);
 
-    while (cur_entry != NULL) {
+    while (cur_entry != NULL)
+    {
         struct cache_entry *next_entry = cur_entry->next;
 
         free_entry(cur_entry);
@@ -122,9 +128,25 @@ void cache_free(struct cache *cache)
  */
 void cache_put(struct cache *cache, char *path, char *content_type, void *content, int content_length)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    struct cache_entry *new = alloc_entry(path, content_type, content, content_length);
+
+    dllist_insert_head(cache, new);
+
+    hashtable_put(cache->index, path, new);
+
+    cache->cur_size++;
+
+    if (cache->cur_size > cache->max_size)
+    {
+        struct cache_entry *last_tail = dllist_remove_tail(cache);
+        hashtable_delete(cache->index, last_tail->path);
+        free_entry(last_tail);
+
+        if (cache->cur_size > cache->max_size)
+        {
+            cache->cur_size--;
+        }
+    }
 }
 
 /**
