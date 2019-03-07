ffb7baa02a1b3c2f38a6add63b0c753e2a92311b
Patrick Thompson <patrick.jthompson93@gmail.com>
2019-03-07 15:47:53 -0800

    finishes day 4 mvp

--- src/cache.c
+++ src/cache.c
@@ -158,7 +158,15 @@ void cache_put(struct cache *cache, char *path, char *content_type, void *conten
  */
 struct cache_entry *cache_get(struct cache *cache, char *path)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    // Attempt to find the cache entry pointer by path in the hash table
+    struct cache_entry *entry = hashtable_get(cache->index, path);
+    // If not found, return NULL
+    if (entry == NULL)
+    {
+        return NULL;
+    }
+    // Move the cache entry to the head of the doubly-linked list
+    dllist_move_to_head(cache, entry);
+    // Return the cache entry pointer
+    return entry;
 }
