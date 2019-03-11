71dd1f45524c30017d8ed6ba732f1a6425fa05c5
AM_Myrick <amyrick.820@gmail.com>
2019-03-11 17:22:02 -0400

    implements arbitrary file serving

--- src/server.c
+++ src/server.c
@@ -142,9 +142,24 @@ void resp_404(int fd)
  */
 void get_file(int fd, struct cache *cache, char *request_path)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    char filepath[4096];
+    struct file_data *filedata; 
+    char *mime_type;
+    
+    snprintf(filepath, sizeof filepath, "%s/%s", SERVER_ROOT, request_path);
+    filedata = file_load(filepath);
+
+    if (filedata == NULL) {
+        // TODO: make this non-fatal
+        fprintf(stderr, "cannot find %s\n", request_path);
+        exit(3);
+    }
+
+    mime_type = mime_type_get(filepath);
+
+    send_response(fd, "HTTP/1.1 200 OK", mime_type, filedata->data, filedata->size);
+
+    file_free(filedata);
 }
 
 /**
@@ -187,7 +202,7 @@ void handle_http_request(int fd, struct cache *cache)
     }
     else
     {
-        resp_404(fd);
+        get_file(fd, cache, path);
     }
     
     ///////////////////
