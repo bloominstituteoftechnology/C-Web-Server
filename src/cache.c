a227f08e7521297caaa7c49926d12cbf54727bc4
Farhan Farooqui <farhan@farooqui.net>
2019-03-13 18:35:32 -0400

    cache methods

--- src/cache.c
+++ src/cache.c
@@ -96,9 +96,13 @@ struct cache_entry *dllist_remove_tail(struct cache *cache)
  */
 struct cache *cache_create(int max_size, int hashsize)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    struct cache *cache = malloc(sizeof(cache));
+
+    cache->index = hashtable_create(max_size, hashsize);
+    cache->max_size = max_size;
+    cache->cur_size = 0;
+
+    return cache;
 }
 
 void cache_free(struct cache *cache)
@@ -160,7 +164,15 @@ void cache_put(struct cache *cache, char *path, char *content_type, void *conten
  */
 struct cache_entry *cache_get(struct cache *cache, char *path)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    struct cache_entry *entry = hashtable_get(cache->index, path);
+
+    if (entry != NULL){
+        dllist_move_to_head(cache, entry);
+        
+    } else {
+        printf("Page not in cache/n");
+    }
+
+    return entry;
+
 }
