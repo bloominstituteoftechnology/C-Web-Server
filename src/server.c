c49aaef2845a141d47f904aa1a7f8aa10ce88960
mparedes003 <onnabugeishadev@gmail.com>
2019-04-15 15:24:34 -0700

    Work on send_response() and handle_http_request()

--- src/server.c
+++ src/server.c
@@ -34,7 +34,7 @@
 #include "mime.h"
 #include "cache.h"
 
-#define PORT "3490"  // the port users will be connecting to
+#define PORT "3490" // the port users will be connecting to
 
 #define SERVER_FILES "./serverfiles"
 #define SERVER_ROOT "./serverroot"
@@ -59,16 +59,44 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
     // IMPLEMENT ME! //
     ///////////////////
 
+    // // time_t is an arithmetic type suitable to represent time
+    // time_t secs;
+    // // struct tm is a structure type for holding components of calendar time
+    // struct tm *time_info;
+
+    // time(&secs);
+    // time_info = localtime(&secs);
+
+    // char *body_str = body;
+
+    // Marker for time
+    time_t time_res_sent = time(NULL);
+
+    // Response length
+    int response_length = sprintf(
+        response, "%s\nDate: %sConnection: close\nContent-Length: %d\nContent-Type: %s\n\n",
+        // header, asctime(time_info),
+        header, asctime(localtime(&time_res_sent)),
+        content_length,
+        content_type);
+
+    // memcpy(void *to, const void *from, size_t n)
+    // Copies n from a memory area pointed to by 'from' to a memory area pointed to by 'to'
+    memcpy(response + response_length, body, content_length);
+
+    // New response_length
+    response_length += content_length;
+
     // Send it all!
     int rv = send(fd, response, response_length, 0);
 
-    if (rv < 0) {
+    if (rv < 0)
+    {
         perror("send");
     }
 
     return rv;
 }
-
 
 /**
  * Send a /d20 endpoint response
@@ -76,7 +104,7 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
-    
+
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
@@ -94,14 +122,15 @@ void get_d20(int fd)
 void resp_404(int fd)
 {
     char filepath[4096];
-    struct file_data *filedata; 
+    struct file_data *filedata;
     char *mime_type;
 
     // Fetch the 404.html file
     snprintf(filepath, sizeof filepath, "%s/404.html", SERVER_FILES);
     filedata = file_load(filepath);
 
-    if (filedata == NULL) {
+    if (filedata == NULL)
+    {
         // TODO: make this non-fatal
         fprintf(stderr, "cannot find system 404 file\n");
         exit(3);
@@ -145,26 +174,54 @@ void handle_http_request(int fd, struct cache *cache)
     const int request_buffer_size = 65536; // 64K
     char request[request_buffer_size];
 
+    // Buffers for the request:
+    char req_type[8];    // GET, POST, etc.
+    char req_path[1024]; // URL path info, for /d20
+
     // Read request
     int bytes_recvd = recv(fd, request, request_buffer_size - 1, 0);
 
-    if (bytes_recvd < 0) {
+    if (bytes_recvd < 0)
+    {
         perror("recv");
         return;
     }
-
 
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
 
-    // Read the first two components of the first line of the request 
- 
+    // Read the first two components of the first line of the request
+    // NOTE: README says Read the three components from the first line of the HTTP header
+    sscanf(request, "%s %s", req_type, req_path);
+
+    printf("\nHTTP Request: \nType:%s \nPath: %s ", req_type, req_path);
+
+    // strcmp() compares the two strings character by character
+    // starting from the first character until the characters in both strings are equal
+    // or a NULL character is encountered
+
     // If GET, handle the get endpoints
+    if (strcmp(req_type, "GET") == 0)
+    {
+        // Check if it's /d20 and handle that special case
+        if (strcmp(req_path, "/d20") == 0)
+        {
+            get_d20(fd);
+        }
+        else
+        {
+            // If path is not /d20, then get file at the path specified
+            get_file(fd, cache, req_path);
+        }
+    }
+    else
+    {
+        // If not GET, respond with 404
+        resp_404(fd);
+    }
 
-    //    Check if it's /d20 and handle that special case
     //    Otherwise serve the requested file by calling get_file()
-
 
     // (Stretch) If POST, handle the post request
 }
@@ -174,7 +231,7 @@ void handle_http_request(int fd, struct cache *cache)
  */
 int main(void)
 {
-    int newfd;  // listen on sock_fd, new connection on newfd
+    int newfd;                          // listen on sock_fd, new connection on newfd
     struct sockaddr_storage their_addr; // connector's address information
     char s[INET6_ADDRSTRLEN];
 
@@ -183,7 +240,8 @@ int main(void)
     // Get a listening socket
     int listenfd = get_listener_socket(PORT);
 
-    if (listenfd < 0) {
+    if (listenfd < 0)
+    {
         fprintf(stderr, "webserver: fatal error getting listening socket\n");
         exit(1);
     }
@@ -193,24 +251,26 @@ int main(void)
     // This is the main loop that accepts incoming connections and
     // responds to the request. The main parent process
     // then goes back to waiting for new connections.
-    
-    while(1) {
+
+    while (1)
+    {
         socklen_t sin_size = sizeof their_addr;
 
         // Parent process will block on the accept() call until someone
         // makes a new connection:
         newfd = accept(listenfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (newfd == -1) {
+        if (newfd == -1)
+        {
             perror("accept");
             continue;
         }
 
         // Print out a message that we got the connection
         inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
+                  get_in_addr((struct sockaddr *)&their_addr),
+                  s, sizeof s);
         printf("server: got connection from %s\n", s);
-        
+
         // newfd is a new socket descriptor for the new connection.
         // listenfd is still listening for new connections.
 
@@ -223,4 +283,3 @@ int main(void)
 
     return 0;
 }
-
