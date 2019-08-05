2ac94863112d4583870b068416b00788b7ef245c
nateyoungren <nathanaelyoungren@gmail.com>
2019-08-05 16:14:01 -0700

    Get file func now returns files in serverroot

--- src/server.c
+++ src/server.c
@@ -112,12 +112,27 @@ void resp_404(int fd)
  */
 void get_file(int fd, struct cache *cache, char *request_path)
 {
-    (void)fd;
     (void)cache;
-    (void)request_path;
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+
+    char filepath[4096];
+    struct file_data *filedata; 
+    char *mime_type;
+
+    snprintf(filepath, sizeof filepath, "%s%s", SERVER_ROOT, request_path);
+    filedata = file_load(filepath);
+
+    printf("%s\n", filepath);
+
+    if (filedata == NULL) {
+        fprintf(stderr, "cannot find file\n");
+        exit(3);
+    }
+
+    mime_type = mime_type_get(filepath);
+
+    send_response(fd, "HTTP/1.1 OK", mime_type, filedata->data, filedata->size);
+
+    file_free(filedata);
 }
 
 /**
